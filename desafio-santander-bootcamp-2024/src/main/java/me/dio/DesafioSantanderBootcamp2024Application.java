2066b7f0420293ce08eb303ce11a17cb78b500a2
alyson <alysoncesarfumagalli@gmail.com>
2024-07-15 15:06:58 -0300

    Criação do projeto usando Gradle

---
+++ desafio-santander-bootcamp-2024/src/main/java/me/dio/DesafioSantanderBootcamp2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSantanderBootcamp2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSantanderBootcamp2024Application.class, args);
+	}
+
+}
